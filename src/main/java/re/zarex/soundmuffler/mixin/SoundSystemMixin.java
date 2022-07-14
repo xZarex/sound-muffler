7618d9509f9d78dcc3289d344056fdae8b37b5a1
xZarex <85762318+xZarex@users.noreply.github.com>
2022-07-14 17:27:51 +0200

    update to 1.19

--- src/main/java/re/zarex/soundmuffler/mixin/SoundSystemMixin.java
+++ src/main/java/re/zarex/soundmuffler/mixin/SoundSystemMixin.java
@@ -5,11 +5,23 @@ import net.minecraft.client.sound.SoundSystem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import re.zarex.soundmuffler.SoundMufflerClientMod;
+import re.zarex.soundmuffler.SoundMufflerMod;
 
 @Mixin(SoundSystem.class)
 public class SoundSystemMixin {
+
+    @Redirect(method = "Lnet/minecraft/client/sound/SoundSystem;play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sound/SoundInstance;getVolume()F"))
+    private float injectedGetVolume(SoundInstance sound) {
+        if (SoundMufflerClientMod.isMufflerClose(sound.getX(), sound.getY(), sound.getZ()))
+        {
+            return 0f;
+        }
+
+        return sound.getVolume();
+    }
 
     @Inject(method = "Lnet/minecraft/client/sound/SoundSystem;getAdjustedVolume(Lnet/minecraft/client/sound/SoundInstance;)F", at = @At("HEAD"), cancellable = true)
     private void getAdjustedVolume(SoundInstance sound, CallbackInfoReturnable<Float> cir) {
